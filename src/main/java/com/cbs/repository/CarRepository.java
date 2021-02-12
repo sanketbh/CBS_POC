c73af8a5ab1a828e9a23cc5ffb727bec65a6d447
sanketbh <sanketbh@cybage.com>
2021-02-12 15:26:57 +0530

    done  changes in pagination Logic to get total Number of items in table with other data

--- src/main/java/com/cbs/repository/CarRepository.java
+++ src/main/java/com/cbs/repository/CarRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,7 +18,7 @@ public interface CarRepository extends PagingAndSortingRepository<Car, Integer> 
 
 	// get all cars with valid insurance
 	@Query("SELECT c FROM Car c WHERE c.insuranceTill >=:insuranceTill")
-	public List<Car> findAllCarWithValidInsurance(@Param("insuranceTill") LocalDateTime insuranceTill,
+	public Page<Car> findAllCarWithValidInsurance(@Param("insuranceTill") LocalDateTime insuranceTill,
 			Pageable pageable);
 
 	public Optional<Car> findByCarId(int carId);
